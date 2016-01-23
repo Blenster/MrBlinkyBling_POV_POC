a2c2f9987eeefcc06c68ff02976b7bc9c5ef6a55
Blenster <blenster@yahoo.com>
2016-01-23 03:28:30 -0500

    Adding code to begin reading folders of images - not functional yet.

--- main.py
+++ main.py
@@ -3,24 +3,56 @@
 # Version 0.1 Alpha -- Rough draft for Proof of Concept
 # Authors Ben Hibben/Blenster and Charles Lehman/The Hat
 # Started: Jan 20 2016
-# Last updated: Jan 20 2016 - Blenster
+# Last updated: Jan 23 2016 - Blenster
+
+# Notes:
+# Assumes square images
+# max data rate 4 Mbps
 
 # libraries and dependencies
-from PIL import Image
+from PIL import Image  # ImageSequence
 
 # variables
 imageName = "test.jpg"
+directoryName = "test01/"  # folder name to search for images in
+# loadMode = "single"  # single | multi
+loadMode = "multi"  # single | multi
+multiMode = "folder"  # folder | zip
+# multiMode = "zip"  # folder | zip
+# TODO variables for Pi to micro-controller data exchange, etc. (resolution, GPIO to set HI/LOW - count, period)
+
+# lists
+multiImageList = []  # holds a list of lists; each list holds a list of pixel RGB tuples
+imageDataList = []  # holds a list of pixel RGB tuples
 
 # constants
 BLACK_THRESHOLD = 15  # The cutoff point where we decide not to light up the LEDs; anything below this is "off"
+# ^^^^^^^^^^^^ This constant may not be used after all; battery conservation isn't too critical on this project
 
 # objects
-img = Image.open(imageName)  # temporary single image - TODO load a folder or ZIP file of images
+# img = Image.open(imageName)  #
 
 # ****  primary code:  ****
 
-# read in image(s);
-# TODO Loop through ZIP or folder here
+# read in image(s)
+if loadMode == "single":
+    img = Image.open(imageName)  # load a single image
+elif loadMode == "multi":
+    if multiMode == "folder":
+        print('Loading a folder of images')
+        # for file in
+    elif multiMode == "zip":
+        print('Loading a zip file')
+    else:
+        print('Error: incorrect argument - "folder" or "zip" expected')
+        quit()
+else:
+    print('Error: incorrect argument - "single" or "multi" expected')
+    quit()
+
+# If we have image data we need to sort it and prepare to output it in a way that the POV display can use
+# convert the cartesian coordinates to polar
+
 
 # read in the RGB values of the images into arrays
 imgData = list(img.getdata())  # test code
